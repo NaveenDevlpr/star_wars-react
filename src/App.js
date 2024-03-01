c797a76b9ca986a723e5bf6c722b28841efe6fba
naveen <naveenmk675@gmail.com>
2024-03-02 00:11:48 +0530

    tailwind integrated

--- src/App.js
+++ src/App.js
@@ -16,39 +16,18 @@ function App() {
 
   }
 
-  
+  resident.map((e)=>{
+    console.log(e)
+  })
   
   useEffect(()=>{
    getData()
   },[])
   return (
-    <div className="App">
-     <div className='' style={{display:'grid',gridTemplateColumns:'1fr 1fr 1fr 1fr'}}>
-        {
-          data.map((e,i)=>{
-            return(
-              <div key={i} style={{display:'flex',flexDirection:'column'}}>
-                  <h2>Name : {e.name}</h2>
-                  <h2>Population : {e.population}</h2>  
-                  <h2>Climate : {e.climate}</h2>    
-                  <h2>
-                  {
-                    resident.map((e,j)=>{
-                      
-                        e[i].map((ele,k)=>{
-                          return(
-                            <div>{ele.name}</div>
-                          )
-                        })
-                      
-                    })
-                  }            
-                  </h2>
-              </div>
-            ) 
-          })
-        }
-     </div>
+    <div className="p-6 w-full">
+        <div className='grid grid-cols-3 w-full'>
+            
+        </div>
     </div>
   );
 }
